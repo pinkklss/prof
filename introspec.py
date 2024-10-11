d074500e17d82a765273d8c730c0a019dd31b724
pinkklss <shaker_04@mail.ru>
2024-10-11 18:37:30 +0700

    Update introspec.py

--- introspec.py
+++ introspec.py
@@ -2,14 +2,17 @@ import pprint
 
 
 def introspection_info(obj):
-    result = {
-        'Атрибуты объекта': getattr(obj, '__methods__', []),
-        'Тип объекта': type(obj),
-        'Методы': dir(obj),
-        'Модуль': obj.__mod__,
-    }
+    attributes = dir(obj)
+    obj_type = type(obj)
+    methods = [method for method in attributes if callable(getattr(obj, method))]
+    module = obj.__class__.__module__
+    
+    result = {'type': obj_type, 'attributes': attributes, 'methods': methods, 'module': module},
     return result
 
 
-number_info = introspection_info('f')
+number_info = introspection_info(5)
 pprint.pprint(number_info)
+
+str_info = introspection_info('f')
+pprint.pprint(str_info)
